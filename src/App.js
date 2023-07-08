865613ea9e1b206efc82a4a3e467672e97c8bf47
Mashood Ali <mashoodali219@gmail.com>
2023-07-08 22:29:45 +0500

    implement Async Thunk feature in redux

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
 import Navbar from "./components/Navbar";
 import Model from "./components/Model";
 import CartContainer from "./components/CartContainer";
-import { calculateTotal } from "./features/cart/cartSlice";
+import { calculateTotal, getCartItems } from "./features/cart/cartSlice";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
 function App() {
   const dispatch = useDispatch();
-  const { cartItems } = useSelector((state) => state.cart);
+  const { cartItems, isLoading } = useSelector((state) => state.cart);
   const { isOpen } = useSelector((state) => state.model);
 
   useEffect(() => {
     dispatch(calculateTotal());
   }, [dispatch, cartItems]);
+
+  useEffect(() => {
+    dispatch(getCartItems("random"));
+  }, []);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <>
       {isOpen && <Model />}
--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,12 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
-import cartItems from "../../cartItems";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import cartItems from "../../cartItems";
+import axios from "axios";
+import { OpenModel } from "../model/modelSlice";
+
+const url = "https://course-api.com/react-useReducer-cart-project";
 
 const initialState = {
-  cartItems: cartItems,
+  cartItems: [],
   amount: 4,
   total: 0,
   isLoading: true,
 };
+
+export const getCartItems = createAsyncThunk(
+  "cart/getCartItems",
+  async (name, thunkAPI) => {
+    try {
+      //? with thunkAPI we can access whole store and can dispatch any feature
+      // console.log(`thunk api = ${thunkAPI} ${name}`);
+      // thunkAPI.dispatch(OpenModel());
+      const res = await axios(url);
+      return res.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue("custom error");
+    }
+  }
+);
 
 const cartSlice = createSlice({
   name: "cart",
@@ -38,6 +57,20 @@ const cartSlice = createSlice({
       });
       state.amount = amount;
       state.total = total;
+    },
+  },
+  extraReducers: {
+    [getCartItems.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [getCartItems.fulfilled]: (state, action) => {
+      console.log(action);
+      state.isLoading = false;
+      state.cartItems = action.payload;
+    },
+    [getCartItems.rejected]: (state, { payload }) => {
+      state.isLoading = false;
+      console.log(payload);
     },
   },
 });
