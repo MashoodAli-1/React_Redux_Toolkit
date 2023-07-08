d6edde77a39b8173daa8e298f9b07f531fae95a8
Mashood Ali <mashoodali219@gmail.com>
2023-07-08 16:59:21 +0500

    complete the cart slice and Model.js component

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -11,8 +11,39 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+    removeItem: (state, action) => {
+      const itemId = action.payload;
+      state.cartItems = state.cartItems.filter((item) => item.id !== itemId);
+    },
+    increase: (state, { payload }) => {
+      const itemId = payload;
+      const cartItem = state.cartItems.find((item) => item.id === itemId);
+      cartItem.amount = cartItem.amount + 1;
+    },
+    decrease: (state, { payload }) => {
+      const itemId = payload;
+      const cartItem = state.cartItems.find((item) => item.id === itemId);
+      cartItem.amount = cartItem.amount - 1;
+    },
+    calculateTotal: (state) => {
+      let amount = 0;
+      let total = 0;
+      state.cartItems.forEach((item) => {
+        amount += item.amount;
+        total += item.amount * item.price;
+      });
+      state.amount = amount;
+      state.total = total;
+    },
+  },
 });
 
 console.log(cartSlice);
 
+export const { clearCart, removeItem, increase, decrease, calculateTotal } =
+  cartSlice.actions;
 export default cartSlice.reducer;
